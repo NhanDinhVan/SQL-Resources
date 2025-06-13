f334f7b30bf4abd23d6d4a273ba317d49514dbb6
DinhVanNhan <vannhan10102004@gmail.com>
2025-06-13 10:58:44 +0700

    Leetcode: 1965. Employees With Missing Information.

---
+++ Leetcode SQL/Easy/_1965_Leetcode_Employees_With_Missing_Information.sql
@@ -0,0 +1,15 @@
+# Write your MySQL query statement below
+select employee_id
+from Employees
+left join Salaries using (employee_id)
+where salary is null
+
+union 
+
+select employee_id
+from Salaries
+left join Employees using (employee_id)
+where name is null
+
+
+ORDER BY employee_id;
