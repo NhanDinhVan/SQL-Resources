ec4b9c5c96c9bdf45940ad9b1af6f741a667b312
DinhVanNhan <vannhan10102004@gmail.com>
2025-06-16 17:08:23 +0700

    Leetcode: 627. Swap Salary.

---
+++ Leetcode SQL/Easy/_627_Leetcode_Swap_Salary.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+-- UPDATE Salary
+-- SET sex = CASE
+--              WHEN sex = 'f' THEN 'm'
+--              WHEN sex = 'm' THEN 'f'
+--           END;
+-- Solution 2
+-- UPDATE Salary
+-- SET sex = IF(sex = 'm', 'f', 'm');
+
